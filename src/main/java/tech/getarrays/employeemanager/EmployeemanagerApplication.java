46839dfd48f1e7b51c19c50fa63dad2bd21ed86f
poolr-ridesharing <84660656+poolr-ridesharing@users.noreply.github.com>
2023-03-08 11:01:05 +0300

    added Cors Configuration

--- src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -1,7 +1,13 @@
 package tech.getarrays.employeemanager;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
@@ -10,4 +16,36 @@ public class EmployeemanagerApplication {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
 
+	@Bean
+	public org.springframework.web.filter.CorsFilter corsFilter(){
+		CorsConfiguration corsConfiguration=new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin"
+				,"Access-Control-Allow-Origin"
+				,"Content-Type"
+				,"Accept"
+				,"Authorization"
+				,"Origin , Accept"
+				,"X-Requested-With"
+				,"Access-Control-Request-Method"
+				,"Access-Control-Request-Headers"
+		));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin"
+				,"Content-Type"
+				,"Accept"
+				,"Authorization"
+				,"Access-Control-Allow-Origin"
+				,"Access-Control-Allow-Origin"
+				,"Access-Control-Allow-Credentials"
+				,"Access-Control-Request-Method"
+				,"Access-Control-Request-Headers"
+
+		));
+
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET","PUT","POST","DELETE"));
+		var urlBasedConfigurationSource=new UrlBasedCorsConfigurationSource();
+		urlBasedConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+		return new org.springframework.web.filter.CorsFilter(urlBasedConfigurationSource);
+	}
 }
